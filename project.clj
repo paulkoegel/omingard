49a60c50129524f8de6feec34dbfba9a5ce001d5
Paul Wittmann <paul@railslove.com>
2014-08-08 20:41:06 +0200

    Project.clj: updated all dependencies and plugins. For some reason we need :exclusions for garden and lein-garden.
    (error message without :exclusions was
    
    lein garden auto
    Compiling Garden...
    (WARNING!!! version ranges found for:)
    ([garden "1.1.5"] -> [com.keminglabs/cljx "0.3.1"] -> [org.clojars.trptcolin/sjacket "0.1.0.3"] -> [org.clojure/clojure "[1.3.0,)"]
    Consider using [garden "1.1.5" :exclusions [org.clojure/clojure]].)
    ([garden "1.1.5"] -> [com.keminglabs/cljx "0.3.1"] -> [org.clojars.trptcolin/sjacket "0.1.0.3"] -> [net.cgrand/regex "1.1.0"] -> [org.clojure/clojure "[1.2.0,)"]
    Consider using [garden "1.1.5" :exclusions [org.clojure/clojure]].)
    ([garden "1.1.5"] -> [com.keminglabs/cljx "0.3.1"] -> [org.clojars.trptcolin/sjacket "0.1.0.3"] -> [net.cgrand/parsley "0.9.1"] -> [org.clojure/clojure "[1.2.0,)"]
    Consider using [garden "1.1.5" :exclusions [org.clojure/clojure]].)
    ([garden "1.1.5"] -> [com.keminglabs/cljx "0.3.1"] -> [org.clojars.trptcolin/sjacket "0.1.0.3"] -> [net.cgrand/parsley "0.9.1"] -> [net.cgrand/regex "1.1.0"] -> [org.clojure/clojure "[1.2.0,)"]
    Consider using [garden "1.1.5" :exclusions [org.clojure/clojure]].)
    nil
    )

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Better Solitaire 'Irmingard' written in Om"
   :url "https://github.com/paulwittmann/omingard"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.0"]
-                 [garden "1.1.5" :exclusions [org.clojure/clojure]]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2277"]
+                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
+                 [om "0.7.1"]
+                 [garden "1.2.1" :exclusions [org.clojure/clojure]]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]
+  :plugins [[lein-cljsbuild "1.0.3"]
             [lein-garden "0.1.9" :exclusions [org.clojure/clojure]]]
 
   :source-paths ["src"]
