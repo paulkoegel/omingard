d2e14ba6eb90e9c5f2af70670d4c158c06e19cd4
Paul Wittmann <paul@railslove.com>
2014-08-18 10:04:39 +0200

    Core.cljs: fixed comment.

--- src/omingard/core.cljs
+++ src/omingard/core.cljs
@@ -1,9 +1,9 @@
-;; 1. Let there be cards in a stack: 1 Rummy deck (52 cards - 2-10, jack, queen, king, ace)
-;; 2. shuffle stack.
-;; 3. serve cards to columns
-;; 4. game begins: either move a free open card from one column to another, discard aces
+;; 1. Let there be cards in a stack: 2 Rummy decks (104 cards - ace, 2-10, jack, queen, king; each suit twice)
+;; 2. Shuffle stack.
+;; 3. Serve cards to columns
+;; 4. Game begins: either move a free open card from one column to another, discard aces
 ;;    (and after them 2s etc.) to one of eight discard piles, or serve new open cards (1 per column)
-;; 5. continue until there are no more moves (you lose) or all cards have been discarded to the piles.
+;; 5. Continue until there are no more moves or all cards have been discarded to the piles.
 
 (ns omingard.core
   (:require-macros [cljs.core.async.macros :refer [go]])
