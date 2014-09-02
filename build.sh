41c2908fdf474355d72060f48e9dc893d9f6d0f6
Paul Wittmann <paul@railslove.com>
2014-09-02 13:15:04 +0200

    build.sh: reset deploy/resoures.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp prod.html ../deploy/index.html
+rm -rf ../deploy/resources # reset resources directory
 mkdir -p ../deploy/resources/public
 cp resources/public/* ../deploy/resources/public
 cp favicon.png ../deploy
