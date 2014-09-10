3e3fc533a9f2096a980641c4026b2c69b8496690
Paul Wittmann <paul@railslove.com>
2014-09-11 00:35:29 +0200

    Added favicons generated by http://realfavicongenerator.net.

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cp prod.html ../deploy/index.html
 rm -rf ../deploy/resources # reset resources directory
 mkdir -p ../deploy/resources/public
 cp resources/public/* ../deploy/resources/public
-cp favicon.png ../deploy
+cp -R favicons/* ../deploy
